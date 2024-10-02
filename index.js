8fd827a8afadc9876be5b85272320b4df82dc299
Karimi-F <fridah37karimi@gmail.com>
2024-10-02 22:11:07 +0300

    Completes the looping lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards (name,string){
+    const newArray = []
+for (let key = 0; key < name.length; key++){
+    newArray.push( `Thank you, ${name[key]}, for the wonderful ${string} gift!`)
+}
+    return newArray
+}
+console.log(writeCards(names,"surprise"));
+
+function countDown(){
+    let number = 10
+    while (number >= 0){
+        console.log(number);
+        number --
+    }
+    
+}
+countDown();
